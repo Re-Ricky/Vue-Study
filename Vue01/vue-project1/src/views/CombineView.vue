b639678cb3d1f2c982042e89e4372facafd0e70c
ricky <958246321@qq.com>
2025-10-30 18:03:33 +0800

    组合式声明API

---
+++ Vue01/vue-project1/src/views/CombineView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {onMounted, ref} from "vue";
+
+const msg = ref("Hello, 组合式API!");
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
+</script>
+
+<template>
+  <h1> {{ msg }} </h1>
+  <input type="button" value="点击+1" @click="increment"> {{ count }}
+</template>
+
+<style scoped>
+
+</style>
---
+++ Vue01/vue-project1/src/views/OptionView.vue
@@ -0,0 +1,29 @@
+<script>
+// 选项式 API
+export default {
+  data() { // 声明响应式对象
+    return {
+      message: "Hello 声明式API",
+      count: 0
+    }
+  },
+  methods: { // 声明方法, 可以通过组件实例访问
+    increment() {
+      this.count++;
+    }
+  },
+
+  mounted() { // 声明钩子函数
+    console.log('页面加载完成');
+  }
+}
+</script>
+
+<template>
+  <h1> {{ message }}</h1>
+  <input type="button" value="点我+1" @click="increment"> {{ count }}
+</template>
+
+<style scoped>
+
+</style>
