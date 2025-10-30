1cc1f949396862023dc8c5043cf31df86469b8df
ricky <958246321@qq.com>
2025-10-30 22:18:10 +0800

    将模板改成组合式声明API

--- Vue01/vue-project1/src/App.vue
+++ Vue01/vue-project1/src/App.vue
@@ -2,14 +2,17 @@
 
 import OptionView from "@/views/OptionView.vue";
 import CombineView from "@/views/CombineView.vue";
+import UserList from "@/views/UserList.vue";
 </script>
 
 <template>
-  <h1> Hello view... </h1>
-  <!--  要展示什么组件就写在这里-->
-  <option-view></option-view>
+<!--  <h1> Hello view... </h1>-->
+<!--  &lt;!&ndash;  要展示什么组件就写在这里&ndash;&gt;-->
+<!--  <option-view></option-view>-->
 
-  <combine-view></combine-view>
+<!--  <combine-view></combine-view>-->
+
+  <user-list></user-list>
 </template>
 
 <style scoped>
