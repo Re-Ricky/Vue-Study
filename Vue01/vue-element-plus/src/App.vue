eaf1de3085714211a3d6da3242cb7b051d0bec64
ricky <958246321@qq.com>
2025-11-01 16:12:47 +0800

    试用 TableView

--- Vue01/vue-element-plus/src/App.vue
+++ Vue01/vue-element-plus/src/App.vue
@@ -1,11 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TableView from './components/element_vue/TableView.vue';
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<table-view></table-view>
 </template>
 
 <style scoped></style>
---
+++ Vue01/vue-element-plus/src/components/element_vue/TableView.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    age: 20,
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
+</script>
+
+<template>
+
+
+  <div class="button-row">
+    <el-button round>Round</el-button>
+    <el-button type="primary" round>Primary</el-button>
+    <el-button type="success" round>Success</el-button>
+    <el-button type="info" round>Info</el-button>
+    <el-button type="warning" round>Warning</el-button>
+    <el-button type="danger" round>Danger</el-button>
+  </div>
+
+  <el-table :data="tableData" stripe style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="age" label="Age" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
+</template>
+
+<style scoped>
+
+</style>
